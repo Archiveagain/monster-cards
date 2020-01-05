4a20e67c7f4eb3a30c8e7274b640205b03969d79
kobike69 <kobiel6@wp.pl>
2020-01-05 13:22:52 +0000

    Updates

---
+++ precache-manifest.2185dfbb5bf5753a45d9b5ea2bc1728a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21464f401372346adbbe96791883f41f",
+    "url": "/monster-cards/index.html"
+  },
+  {
+    "revision": "c1d80db9b97077472206",
+    "url": "/monster-cards/static/css/main.0c523ca9.chunk.css"
+  },
+  {
+    "revision": "a8b76958ccd5d6d253a3",
+    "url": "/monster-cards/static/js/2.7029a57e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-cards/static/js/2.7029a57e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1d80db9b97077472206",
+    "url": "/monster-cards/static/js/main.52861c7f.chunk.js"
+  },
+  {
+    "revision": "9786c0bbe9c648498108",
+    "url": "/monster-cards/static/js/runtime-main.730fcdb8.js"
+  }
+]);
